ec91477f4be0247870628b6ee72a62d7d48aa0e8
Doug Knight <dougbknight@gmail.com>
2015-11-30 21:42:17 -0600

    start package pricing implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'pages/welcome'
-  get 'pages/faq'
+  get 'pages/package_pricing'
+  get 'pages/a_la_carte_pricing'
+  get 'pages/faqs'
   get 'pages/about_us'
+  get 'pages/contact_us'
   root to: 'pages#welcome'
 end
