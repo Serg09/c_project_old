48cd4f605d73798431c6c376ff2609ce70f3b8ea
Doug Knight <dougbknight@gmail.com>
2016-01-22 21:28:43 -0600

    add previews for author mailers

--- app/mailers/author_mailer.rb
+++ app/mailers/author_mailer.rb
@@ -2,17 +2,26 @@ class AuthorMailer < ApplicationMailer
   default from: 'accounts@crowdscribe.com'
 
   def account_pending_notification(author)
+    inline_images
     @author = author
     mail to: author.email, subject: 'Approval pending'
   end
 
   def account_approved_notification(author)
+    inline_images
     @author = author
     mail to: author.email, subject: 'Account approved'
   end
 
   def account_rejected_notification(author)
+    inline_images
     @author = author
     mail to: author.email, subject: 'Account rejected'
+  end
+
+  private
+
+  def inline_images
+    attachments.inline['logo.png'] = File.read('app/assets/images/crowdscribed_logo.png')
   end
 end
--- spec/mailers/previews/author_mailer_preview.rb
+++ spec/mailers/previews/author_mailer_preview.rb
@@ -1,4 +1,17 @@
 # Preview all emails at http://localhost:3000/rails/mailers/author_mailer
 class AuthorMailerPreview < ActionMailer::Preview
+  def account_pending_notification
+    author = Author.pending.first || FactoryGirl.create(:author, status: Author.PENDING)
+    AuthorMailer.account_pending_notification(author)
+  end
 
+  def account_approved_notification
+    author = Author.accepted.first || FactoryGirl.create(:author, status: Author.ACCEPTED)
+    AuthorMailer.account_approved_notification(author)
+  end
+
+  def account_rejected_notification
+    author = Author.rejected.first || FactoryGirl.create(:author, status: Author.REJECTED)
+    AuthorMailer.account_rejected_notification(author)
+  end
 end
