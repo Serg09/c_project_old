7726ca619c5dbc919b39e02f511d1823769c5b7a
Doug Knight <dougbknight@gmail.com>
2016-01-25 16:37:04 -0600

    remove link model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160125033441) do
+ActiveRecord::Schema.define(version: 20160125031201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,14 +89,6 @@ ActiveRecord::Schema.define(version: 20160125033441) do
     t.boolean  "archived",   default: false, null: false
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
-  end
-
-  create_table "links", force: :cascade do |t|
-    t.integer  "bio_id",                 null: false
-    t.string   "site",       limit: 20,  null: false
-    t.string   "url",        limit: 255, null: false
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
   end
 
 end
--- spec/factories/links.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :link do
-    bio
-    site 'facebook'
-    url { Faker::Internet.url('facebook.com') }
-  end
-end
