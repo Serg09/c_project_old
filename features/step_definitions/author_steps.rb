52cd9ffa2c4a65e7bd9a86db49799b4da28d02a2
Doug Knight <dougbknight@gmail.com>
2016-01-27 20:14:41 -0600

    consolidate author transformation definitions

--- features/step_definitions/author_steps.rb
+++ features/step_definitions/author_steps.rb
@@ -1,9 +1,3 @@
-AUTHOR = Transform /author (.+@.+)/ do |email|
-  author = Author.find_by(email: email)
-  expect(author).not_to be_nil
-  author
-end
-
 When /^an administrator approves the account for (#{AUTHOR})$/ do |author|
   author.status = Author.ACCEPTED
   author.save!
