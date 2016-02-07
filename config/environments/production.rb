b16baf5bc199a903465c1f4984f6a20e2a03d8dc
Doug Knight <dougbknight@gmail.com>
2016-02-06 21:18:04 -0600

    use environment variables for mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'www.crowdscribed.com', port: 80 }
+  config.action_mailer.default_url_options = { host: ENV['WEBSITE_HOST'], port: ENV['WEBSITE_PORT'] || 80 }
   config.action_mailer.smtp_settings = {
     address:               'smtp.gmail.com',
     port:                  587,
