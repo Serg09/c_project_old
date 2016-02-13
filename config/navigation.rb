55f575ce2e1791f87bbeea3287c90f050e3afdd1
Doug Knight <dougbknight@gmail.com>
2016-02-13 11:25:44 -0600

    finish configuring the navigation

--- config/navigation.rb
+++ config/navigation.rb
@@ -6,6 +6,18 @@ SimpleNavigation::Configuration.run do |navigation|
 
     primary.item :about, 'About', pages_about_us_path do |about|
       about.item :package_pricing, 'Package Pricing', pages_package_pricing_path
+      about.item :a_la_carte, 'A La Carte Pricing', pages_a_la_carte_pricing_path
+      about.item :faqs, 'FAQs', pages_faqs_path
+      about.item :about_us, 'About Us', pages_about_us_path
+      about.item :contact_us, 'Contact Us', new_inquiry_path
+      about.item :book_incubator, 'Book Incubator', pages_book_incubator_path
     end
+    primary.item :authors, 'Authors' do |authors|
+      authors.item :bio, 'Bio', bios_path, if: ->{author_signed_in?}
+      authors.item :author_sign_in, 'Sign In', new_author_session_path, if: ->{!author_signed_in? && !AppSetting.sign_in_disabled?}
+      authors.item :author_sign_up, 'Sign Up', new_author_registration_path, if: ->{!author_signed_in? && !AppSetting.sign_in_disabled?}
+      authors.item :author_sign_out, 'Sign Out', destroy_author_session_path, method: :delete, if: ->{author_signed_in? && !AppSetting.sign_in_disabled?}
+    end
+    primary.item :books, 'Books', pages_books_path
   end
 end
