3a571ee5e3d3baf3b00713a27709e0656befadc5
Doug Knight <dougbknight@gmail.com>
2016-02-13 15:13:35 -0600

    annotate models

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: books
+#
+#  id                :integer          not null, primary key
+#  author_id         :integer          not null
+#  title             :string(255)      not null
+#  short_description :string(1000)     not null
+#  long_description  :text
+#  cover_image_id    :integer
+#  status            :string           default("pending"), not null
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
+
 class Book < ActiveRecord::Base
   validates_presence_of :author_id, :title, :short_description
   validates :title, length: { maximum: 255 }
