ed4994f768390409e1f42f70cf438066b42b1bee
Doug Knight <dougbknight@gmail.com>
2016-02-14 16:43:13 -0600

    add image handling logic to Book

--- app/models/book.rb
+++ app/models/book.rb
@@ -14,9 +14,22 @@
 #
 
 class Book < ActiveRecord::Base
+  belongs_to :author
+
+  before_save :process_cover_image_file
+
   validates_presence_of :author_id, :title, :short_description
   validates :title, length: { maximum: 255 }
   validates :short_description, length: { maximum: 1000 }
 
   attr_accessor :cover_image_file
+
+  private
+
+  def process_cover_image_file
+    return unless cover_image_file
+
+    image = Image.find_or_create_from_file(cover_image_file, author)
+    self.cover_image_id = image.id
+  end
 end
