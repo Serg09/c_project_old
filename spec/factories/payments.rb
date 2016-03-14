5fbf791ef0f066f24e35544412915bb020890899
Doug Knight <dougbknight@gmail.com>
2016-03-14 17:40:31 -0500

    correct bad syntax in payments factory

--- spec/factories/payments.rb
+++ spec/factories/payments.rb
@@ -3,6 +3,6 @@ FactoryGirl.define do
     donation donation
     external_id "PAY-17S8410768582940NKEE66EQ"
     state "approved"
-    content {id: "PAY-17S8410768582940NKEE66EQ"}.to_json
+    content { {id: "PAY-17S8410768582940NKEE66EQ"}.to_json }
   end
 end
