243872b789c17baf1326d086bd2d63b695cba69f
Doug Knight <dougbknight@gmail.com>
2016-04-04 15:33:06 -0500

    implement DonationCollector#perform

--- app/models/donation_collector.rb
+++ app/models/donation_collector.rb
@@ -2,4 +2,12 @@
 # transactions that have already been authorized
 class DonationCollector
   @queue = :donation_collection
+
+  def self.perform(campaign_id)
+    campaign = Campaign.find(campaign_id)
+    campaign.collect_donations
+    CampaignMailer.collection_complete(campaign).deliver_now
+  rescue => e
+    Rails.logger.error "Unable to complete the collection for campaign_id=#{campaign_id}, #{e.message}, #{e.backtrace.join("\n  ")}"
+  end
 end
