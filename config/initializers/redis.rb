f8dcd902583e396c26918b99cdaa41f7a80d7d8d
Doug Knight <dougbknight@gmail.com>
2016-04-18 12:08:14 -0500

    out trace output for redis initializer

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,8 @@
-if ENV['REDISCLOUD_URL']
+if ENV['REDISCLOUD_URL'].present?
+
+  Rails.logger.debug "Using specified redis URL #{ENV['REDISCLOUD_URL']}"
+
   $redis = Redis.new(url: ENV['REDISCLOUD_URL'])
+else
+  Rails.logger.debug "Using default redis configuration"
 end
