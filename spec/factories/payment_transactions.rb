ed48ae1be2e46476f7d28818220c87e5292673b9
Doug Knight <dougbknight@gmail.com>
2016-04-18 17:47:23 -0500

    add transaction details to payment factories

--- spec/factories/payment_transactions.rb
+++ spec/factories/payment_transactions.rb
@@ -7,6 +7,28 @@ FactoryGirl.define do
       {
         id: Faker::Number.hexadecimal(20),
         state: state,
+        intent: 'sale',
+        payer: {
+          payment_method: 'credit_card',
+          funding_instruments: [{
+            credit_card: {
+              type: 'visa',
+              number: 'xxxxxxxxxxxx1111',
+              expire_month: '12',
+              expire_year: '2020',
+              first_name: 'John',
+              last_name: 'Doe',
+              billing_address: {
+                line1: '1234 Main St',
+                line2: 'Apt 227',
+                city: 'Dallas',
+                state: 'TX',
+                postal_code: '75200',
+                country_code: 'US'
+              }
+            }
+          }]
+        }
       }.to_json
     end
 
--- spec/factories/payments.rb
+++ spec/factories/payments.rb
@@ -11,6 +11,27 @@ FactoryGirl.define do
         id: payment.external_id,
         state: payment.state,
         intent: 'sale',
+        payer: {
+          payment_method: 'credit_card',
+          funding_instruments: [{
+            credit_card: {
+              type: 'visa',
+              number: 'xxxxxxxxxxxx1111',
+              expire_month: '12',
+              expire_year: '2020',
+              first_name: 'John',
+              last_name: 'Doe',
+              billing_address: {
+                line1: '1234 Main St',
+                line2: 'Apt 227',
+                city: 'Dallas',
+                state: 'TX',
+                postal_code: '75200',
+                country_code: 'US'
+              }
+            }
+          }]
+        },
         transactions: [{
           amount: {
             total: payment.donation.amount,
