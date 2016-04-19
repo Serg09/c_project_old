bcb0ee842920c68c34672673f793004d2d2ab202
Doug Knight <dougbknight@gmail.com>
2016-04-19 15:15:23 -0500

    adjust donation steps for fulfillment

--- features/step_definitions/donation_steps.rb
+++ features/step_definitions/donation_steps.rb
@@ -3,18 +3,22 @@ Given /^the (#{CAMPAIGN}) has received the following donations$/ do |campaign, t
   table.raw.drop(1).each do |hash|
     values = Hash[keys.zip(hash)]
 
-    reward_description = values.delete(:reward)
-    if reward_description.present?
-      reward = campaign.rewards.find_by(description: reward_description)
-      expect(reward).not_to be_nil
-      values[:reward_id] = reward.id
-    end
-
     created_at = values.delete(:date)
     if created_at.present?
       values[:created_at] = Chronic.parse(created_at)
     end
+    reward_description = values.delete(:reward)
     donation = FactoryGirl.create(:donation, values.merge(campaign: campaign))
     FactoryGirl.create(:payment, donation: donation)
+
+    if reward_description.present?
+      reward = campaign.rewards.find_by(description: reward_description)
+      expect(reward).not_to be_nil
+      if reward.physical_address_required?
+        FactoryGirl.create(:physical_fulfillment, donation: donation, reward: reward)
+      else
+        FactoryGirl.create(:electronic_fulfillment, donation: donation, reward: reward)
+      end
+    end
   end
 end
