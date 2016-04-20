eec1cbb68e4095d990cf72b9c5e48bec13fe2192
Doug Knight <dougbknight@gmail.com>
2016-04-20 12:05:10 -0500

    add FulfillmentsController#fulfill

--- app/controllers/admin/fulfillments_controller.rb
+++ app/controllers/admin/fulfillments_controller.rb
@@ -1,5 +1,6 @@
 class Admin::FulfillmentsController < ApplicationController
   before_filter :safe_authenticate_administrator!
+  before_filter :load_fulfillment, only: :fulfill
 
   layout 'admin'
   respond_to :html
@@ -11,5 +12,17 @@ class Admin::FulfillmentsController < ApplicationController
       ready.
       paginate(page: params[:page])
     respond_with @fulfillments
+  end
+
+  def fulfill
+    @fulfillment.delivered = true
+    flash[:notice] = 'The reward was updated successfully.' if @fulfillment.save
+    redirect_to admin_fulfillments_path
+  end
+
+  private
+
+  def load_fulfillment
+    @fulfillment = Fulfillment.find(params[:id])
   end
 end
--- spec/controllers/admin/fulfillments_controller_spec.rb
+++ spec/controllers/admin/fulfillments_controller_spec.rb
@@ -11,6 +11,22 @@ RSpec.describe Admin::FulfillmentsController, type: :controller do
         expect(response).to have_http_status :success
       end
     end
+
+    describe 'patch :fulfill' do
+      let!(:fulfillment) { FactoryGirl.create(:electronic_fulfillment) }
+
+      it 'redirects to the index page' do
+        patch :fulfill, id: fulfillment
+        expect(response).to redirect_to admin_fulfillments_path
+      end
+
+      it 'marks the record has fulfilled' do
+        expect do
+          patch :fulfill, id: fulfillment
+          fulfillment.reload
+        end.to change(fulfillment, :delivered).from(false).to(true)
+      end
+    end
   end
 
   context 'for an unauthenticated user' do
@@ -18,6 +34,22 @@ RSpec.describe Admin::FulfillmentsController, type: :controller do
       it 'redirects to the welcome page' do
         get :index
         expect(response).to redirect_to root_path
+      end
+    end
+
+    describe 'patch :fulfill' do
+      let!(:fulfillment) { FactoryGirl.create(:electronic_fulfillment) }
+
+      it 'redirects to the welcome page' do
+        patch :fulfill, id: fulfillment
+        expect(response).to redirect_to root_path
+      end
+
+      it 'does not update the fulfillment' do
+        expect do
+          patch :fulfill, id: fulfillment
+          fulfillment.reload
+        end.not_to change(fulfillment, :delivered)
       end
     end
   end
