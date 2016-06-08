92821d5426edfdb813f083e6000bde26e8b65b1f
Doug Knight <doug.knight@crowdscribed.com>
2016-06-08 17:37:54 -0500

    delete rewards when deleting a campaign

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -16,7 +16,7 @@
 class Campaign < ActiveRecord::Base
   belongs_to :book
   has_many :donations
-  has_many :rewards
+  has_many :rewards, dependent: :destroy
 
   validates_presence_of :book_id, :target_date, :target_amount
   validates_numericality_of :target_amount, greater_than: 0
