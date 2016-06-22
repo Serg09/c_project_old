9d41e6e3671314da5f25c0120ef422c712069cd6
Doug Knight <doug.knight@crowdscribed.com>
2016-06-22 15:20:00 -0500

    relax null constraint on payments.external_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160621161100) do
+ActiveRecord::Schema.define(version: 20160622200640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -190,7 +190,7 @@ ActiveRecord::Schema.define(version: 20160621161100) do
 
   create_table "payments", force: :cascade do |t|
     t.integer  "contribution_id", null: false
-    t.string   "external_id",     null: false
+    t.string   "external_id"
     t.string   "state",           null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
