5ded5f57978012fb2eb1916bfc76543181ca807b
Doug Knight <doug.knight@crowdscribed.com>
2016-07-09 13:54:49 -0500

    add UI support for HouseReward#estimator_class

--- app/helpers/admin/house_rewards_helper.rb
+++ app/helpers/admin/house_rewards_helper.rb
@@ -1,2 +1,5 @@
 module Admin::HouseRewardsHelper
+  def estimator_class_options_for_select(selected)
+    options_for_select([['None', ''], ['Publishing', 'PublishingCostEstimator']], selected)
+  end
 end
