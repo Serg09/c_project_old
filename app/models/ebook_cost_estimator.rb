0035a92ddfd1b70b4970a8c2a8918514623e4fde
Doug Knight <dougbknight@gmail.com>
2016-07-15 17:50:37 -0500

    e-book cost is zero for zero books

--- app/models/ebook_cost_estimator.rb
+++ app/models/ebook_cost_estimator.rb
@@ -3,6 +3,7 @@ class EbookCostEstimator
   AVERAGE_CONVERSION = 130.0
 
   def estimate(book_count)
+    return 0 unless book_count > 0
     ANNUAL_DISTRIBUTION + AVERAGE_CONVERSION
   end
 end
--- spec/models/ebook_cost_estimator_spec.rb
+++ spec/models/ebook_cost_estimator_spec.rb
@@ -2,8 +2,15 @@ require 'rails_helper'
 
 describe EbookCostEstimator do
   describe '#estimate' do
-    it 'returns 168' do
-      expect(EbookCostEstimator.new.estimate(10)).to eq 168
+    context 'for a book count of zero' do
+      it 'returns 0' do
+        expect(EbookCostEstimator.new.estimate(0)).to eq 0
+      end
+    end
+    context 'for a non-zero book count' do
+      it 'returns 168' do
+        expect(EbookCostEstimator.new.estimate(10)).to eq 168
+      end
     end
   end
 end
