bd4731d48f1ec611c949e111842ffb07defc6f68
Doug Knight <dougbknight@gmail.com>
2016-07-14 19:27:45 -0500

    account for 1-time and annual physical publishing costs

--- app/models/publishing_cost_estimator.rb
+++ app/models/publishing_cost_estimator.rb
@@ -3,9 +3,18 @@ class PublishingCostEstimator
   ADDITIVE = 5.5
   MULTIPLIER = 1.3
 
+  TITLE_SETUP = 95.0
+  PROOF = 45.0
+  FILE_REVISIONS = 50.0
+  ANNUAL_MARKET_DISTRIBUTION = 15.0
+
   def estimate(book_count)
     additive = ADDITIVE / (MULTIPLIER ** book_count)
     price = BASE_PRICE + additive
-    book_count * price
+    book_count * price +
+      TITLE_SETUP +
+      PROOF +
+      FILE_REVISIONS +
+      ANNUAL_MARKET_DISTRIBUTION
   end
 end
--- spec/models/publishing_cost_estimator_spec.rb
+++ spec/models/publishing_cost_estimator_spec.rb
@@ -1,24 +1,34 @@
 require 'rails_helper'
 
 describe PublishingCostEstimator do
+
+  # From Pricing Model spreadsheet
+  #
+  # Title Setup                         95.00
+  # Physical Book Proof Hardback        45.00
+  # Revision to Cover & Interior Files  50.00
+  # Yearly Print Market Distribution    15.00
+  #------------------------------------------
+  #                                    205.00
+
   describe '#estimate' do
     tests = [
-      { book_count: 1,   expected: 9.61 },
-      { book_count: 2,   expected: 14.32 },
-      { book_count: 3,   expected: 19.03 },
-      { book_count: 4,   expected: 23.74 },
-      { book_count: 5,   expected: 28.45 },
-      { book_count: 6,   expected: 33.16 },
-      { book_count: 7,   expected: 37.87 },
-      { book_count: 8,   expected: 42.58 },
-      { book_count: 9,   expected: 47.29 },
-      { book_count: 10,  expected: 52.00 },
-      { book_count: 15,  expected: 75.55 },
-      { book_count: 20,  expected: 97.87 },
-      { book_count: 25,  expected: 119.95 },
-      { book_count: 50,  expected: 238.27 },
-      { book_count: 75,  expected: 356.57 },
-      { book_count: 100, expected: 451.87 }
+      { book_count: 1,   expected: 214.61 },
+      { book_count: 2,   expected: 219.32 },
+      { book_count: 3,   expected: 224.03 },
+      { book_count: 4,   expected: 228.74 },
+      { book_count: 5,   expected: 233.45 },
+      { book_count: 6,   expected: 238.16 },
+      { book_count: 7,   expected: 242.87 },
+      { book_count: 8,   expected: 247.58 },
+      { book_count: 9,   expected: 252.29 },
+      { book_count: 10,  expected: 257.00 },
+      { book_count: 15,  expected: 280.55 },
+      { book_count: 20,  expected: 302.87 },
+      { book_count: 25,  expected: 324.95 },
+      { book_count: 50,  expected: 443.27 },
+      { book_count: 75,  expected: 561.57 },
+      { book_count: 100, expected: 656.87 }
     ]
     tests.each do |test|
       it "returns a value close to #{test[:expected]} when given #{test[:book_count]}" do
