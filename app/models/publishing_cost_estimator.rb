71a8d3d59b725ad1927141fb25416802c2798d34
Doug Knight <dougbknight@gmail.com>
2016-07-14 19:42:08 -0500

    add 25% markup to publish cost est.

--- app/models/publishing_cost_estimator.rb
+++ app/models/publishing_cost_estimator.rb
@@ -3,6 +3,8 @@ class PublishingCostEstimator
   ADDITIVE = 5.5
   MULTIPLIER = 1.3
 
+  MARKUP_FACTOR = 1.25
+
   TITLE_SETUP = 95.0
   PROOF = 45.0
   FILE_REVISIONS = 50.0
@@ -10,7 +12,7 @@ class PublishingCostEstimator
 
   def estimate(book_count)
     additive = ADDITIVE / (MULTIPLIER ** book_count)
-    price = BASE_PRICE + additive
+    price = (BASE_PRICE + additive) * MARKUP_FACTOR
     book_count * price +
       TITLE_SETUP +
       PROOF +
--- spec/models/publishing_cost_estimator_spec.rb
+++ spec/models/publishing_cost_estimator_spec.rb
@@ -13,27 +13,27 @@ describe PublishingCostEstimator do
 
   describe '#estimate' do
     tests = [
-      { book_count: 1,   expected: 214.61 },
-      { book_count: 2,   expected: 219.32 },
-      { book_count: 3,   expected: 224.03 },
-      { book_count: 4,   expected: 228.74 },
-      { book_count: 5,   expected: 233.45 },
-      { book_count: 6,   expected: 238.16 },
-      { book_count: 7,   expected: 242.87 },
-      { book_count: 8,   expected: 247.58 },
-      { book_count: 9,   expected: 252.29 },
-      { book_count: 10,  expected: 257.00 },
-      { book_count: 15,  expected: 280.55 },
-      { book_count: 20,  expected: 302.87 },
-      { book_count: 25,  expected: 324.95 },
-      { book_count: 50,  expected: 443.27 },
-      { book_count: 75,  expected: 561.57 },
-      { book_count: 100, expected: 656.87 }
+      { book_count: 1,   expected: 215.91 },
+      { book_count: 2,   expected: 224.39 },
+      { book_count: 3,   expected: 231.26 },
+      { book_count: 4,   expected: 237.13 },
+      { book_count: 5,   expected: 242.38 },
+      { book_count: 6,   expected: 247.30 },
+      { book_count: 7,   expected: 252.04 },
+      { book_count: 8,   expected: 256.74 },
+      { book_count: 9,   expected: 261.46 },
+      { book_count: 10,  expected: 266.24 },
+      { book_count: 15,  expected: 291.39 },
+      { book_count: 20,  expected: 318.22 },
+      { book_count: 25,  expected: 345.87 },
+      { book_count: 50,  expected: 486.25 },
+      { book_count: 75,  expected: 626.88 },
+      { book_count: 100, expected: 767.50 }
     ]
     tests.each do |test|
       it "returns a value close to #{test[:expected]} when given #{test[:book_count]}" do
         estimator = PublishingCostEstimator.new
-        expect(estimator.estimate(test[:book_count])).to be_close_to test[:expected], 0.15
+        expect(estimator.estimate(test[:book_count])).to be_close_to test[:expected], 0.05
       end
     end
   end
