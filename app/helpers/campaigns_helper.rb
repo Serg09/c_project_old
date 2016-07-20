749a8bff31195ba13e73dde8fcc160720baf5f1b
Doug Knight <dougbknight@gmail.com>
2016-07-19 20:21:06 -0500

    add UI queues for starting a campaign
    if the author has no approved bio:
      disable start buttons
      display text indicating he/she needs an approved bio

--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -9,6 +9,30 @@ module CampaignsHelper
     end
   end
 
+  def start_campaign_path(campaign)
+    if campaign.author_ready?
+      terms_campaign_path(campaign)
+    else
+      '#'
+    end
+  end
+
+  def start_campaign_title(campaign)
+    if campaign.author_ready?
+      'Click here to start the campaign.'
+    else
+      'You must have an approved bio before you can start the campaign.'
+    end
+  end
+
+  def start_campaign_button_class(campaign)
+    if campaign.author_ready?
+      'btn-success'
+    else
+      'btn-default'
+    end
+  end
+
   DEFAULT_PROGRESS_METER_OPTIONS = { width: 100, height: 300 }
   def render_progress_meter(progress, options = {})
     options = DEFAULT_PROGRESS_METER_OPTIONS.
