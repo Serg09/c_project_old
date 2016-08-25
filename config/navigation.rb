357f20aa9328a815c6db365f57299caf4a5e9527
Doug Knight <doug.knight@crowdscribed.com>
2016-08-25 16:54:30 -0500

    rework navigation

--- config/navigation.rb
+++ config/navigation.rb
@@ -5,30 +5,38 @@ SimpleNavigation::Configuration.run do |navigation|
     primary.dom_class = 'nav navbar-nav' # TODO set a reasonable default in the renderer
 
     primary.item :about, 'About', pages_about_us_path do |about|
-      about.item :package_pricing, 'Package Pricing', pages_package_pricing_path
-      about.item :a_la_carte, 'A La Carte Pricing', pages_a_la_carte_pricing_path
-      about.item :faqs, 'FAQs', pages_faqs_path
       about.item :about_us, 'About Us', pages_about_us_path
       about.item :contact_us, 'Contact us', new_inquiry_path
       about.item :our_team, 'Our team', pages_our_team_path
-      about.item :book_incubator, 'Book Incubator', pages_book_incubator_path
     end
-    primary.item :authors, 'Authors', browse_authors_path
-    primary.item :books, 'Books', browse_books_path
+    primary.item :services, 'Services' do |services_item|
+      services_item.item :package_pricing, 'Package Pricing', pages_package_pricing_path
+      services_item.item :a_la_carte, 'A La Carte Pricing', pages_a_la_carte_pricing_path
+      services_item.item :book_incubator, 'Book Incubator', pages_book_incubator_path
+      services_item.item :faqs, 'FAQs', pages_faqs_path
+    end
+    primary.item :readers, 'Readers' do |readers_item|
+      readers_item.item :browse_authors, 'Browse authors', browse_authors_path
+      readers_item.item :browse_books, 'Browse books', browse_books_path
+    end
     if AppSetting.sign_in_disabled?
       primary.item :subscribe, 'Sign up', new_subscriber_path
     else
-      if user_signed_in?
-        primary.item :profile, 'Profile' do |profile_item|
-          profile_item.item :view_profile, 'View', user_path(current_user)
-          profile_item.item :bio, 'Bio', bios_path
-          profile_item.item :my_books, 'My books', books_path
-          profile_item.item :reward_fulfillment, 'Reward fulfillment', fulfillments_path
-          profile_item.item :author_sign_out, 'Sign Out', destroy_user_session_path, method: :delete, if: ->{user_signed_in? && !AppSetting.sign_in_disabled?}
+      primary.item :authors, 'Authors' do |authors_item|
+        if user_signed_in?
+          authors_item.item :view_profile, 'Profile', user_path(current_user)
+          authors_item.item :my_bio, 'My bio', bios_path
+          authors_item.item :my_books, 'My books', books_path
+          authors_item.item :reward_fulfillment, 'Reward fulfillment', fulfillments_path
+        else
+          authors_item.item :author_sign_up, 'Signup', new_user_registration_path
         end
+      end
+      if user_signed_in?
+        primary.item :author_sign_out, 'Sign Out', destroy_user_session_path, method: :delete
       else
         primary.item :author_sign_in, 'Log in', new_user_session_path
-        primary.item :author_sign_up, 'Signup', new_user_registration_path, if: ->{!user_signed_in? && !AppSetting.sign_in_disabled?}
+        primary.item :author_sign_up, 'Signup', new_user_registration_path
       end
     end
   end
