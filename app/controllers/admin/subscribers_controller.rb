45a62127fc24e46b0350cb607da7bf4f4fcd788e
Doug Knight <dougbknight@gmail.com>
2016-08-26 19:01:13 -0500

    add subscriber CSV download
    [Finishes #129080973]

--- app/controllers/admin/subscribers_controller.rb
+++ app/controllers/admin/subscribers_controller.rb
@@ -1,8 +1,20 @@
+require 'csv'
+
 class Admin::SubscribersController < ApplicationController
+  respond_to :html, :csv
   before_filter :authenticate_administrator!
   layout 'admin'
 
   def index
-    @subscribers = Subscriber.paginate(page: params[:page])
+    @subscribers = Subscriber.all
+    respond_to do |format|
+      format.html do
+        @subscribers = @subscribers.paginate(page: params[:page], per_page: 5)
+      end
+      format.csv do
+        headers['Content-Disposition'] = 'attachment; filename="subscribers.csv"'
+        headers['Content-Type'] = 'text/csv'
+      end
+    end
   end
 end
